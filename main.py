9ecd8f5290e82d87fdacdbd93288e15da37fbc0c
Vu Huy <vuhuycto@gmail.com>
2021-03-31 11:57:27 +0700

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+"""
+Check a number whether is even or not
+"""
 
+
+def main():
+    number = int(input())
+
+    if number % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+if __name__ == '__main__':
+    main()
--- test_main.py
+++ test_main.py
@@ -1,1 +1,7 @@
+import main
 
+
+def test_program(capfd):
+    main.input = lambda: "3"
+    out, err = capfd.readouterr()
+    return out == "Odd"
